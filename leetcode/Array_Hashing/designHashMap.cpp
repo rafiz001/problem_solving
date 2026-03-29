2878316e7373818b97fd0f8083ee16c63c207b20
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2026-03-29 20:55:35 +0600

    fixed bug of remove() of designHashMap

--- leetcode/Array_Hashing/designHashMap.cpp
+++ leetcode/Array_Hashing/designHashMap.cpp
@@ -6,53 +6,65 @@ using namespace std;
 
 class MyHashMap {
 private:
-  const static int TOTAL_BUCKET = 100000;
+    const static int TOTAL_BUCKET = 10004;
 
-  vector<pair<int, int>> fizMap[TOTAL_BUCKET];
+        vector<pair<int, int>> fizMap[TOTAL_BUCKET];
 
-  int getBucketIndex(int key) { return (key + 1 * 7) % TOTAL_BUCKET; }
+    int getBucketIndex(int key) {
+        return (key+1*3) % TOTAL_BUCKET;
+    }
 
 public:
-  MyHashMap() {
-    for (auto bucket : fizMap) {
-      bucket.clear();
+    MyHashMap() {
+        for (auto bucket : fizMap) {
+            bucket.clear();
+        }
     }
-  }
 
-  void put(int key, int value) {
-    int bucketIndex = getBucketIndex(key);
-    for (auto &entry : fizMap[bucketIndex]) {
-      if (entry.first == key) {
-        // found and replacing
-        entry.second = value;
-        return;
-      }
+    void put(int key, int value) {
+        int bucketIndex = getBucketIndex(key);
+        for (auto& entry : fizMap[bucketIndex]) {
+            if (entry.first == key) {
+                // found and replacing
+                entry.second = value;
+                return;
+            }
+        }
+            fizMap[bucketIndex].push_back({key, value});
+        
     }
-    fizMap[bucketIndex].push_back({key, value});
-  }
 
-  int get(int key) {
-    int bucketIndex = getBucketIndex(key);
-    for (auto entry : fizMap[bucketIndex]) {
-      if (entry.first == key) {
-        // found
-        return entry.second;
-      }
+    int get(int key) {
+        int bucketIndex = getBucketIndex(key);
+        for (auto entry : fizMap[bucketIndex]) {
+            if (entry.first == key) {
+                // found
+                return entry.second;
+            }
+        }
+        return -1;
     }
-    return -1;
-  }
 
-  void remove(int key) {
-    int bucketIndex = getBucketIndex(key);
-    for (auto entry : fizMap[bucketIndex]) {
-      if (entry.first == key) {
-        // found
-        swap(entry, fizMap[bucketIndex].back());
-        fizMap[bucketIndex].pop_back();
-      }
+    void remove(int key) {
+        int bucketIndex = getBucketIndex(key);
+        for (auto &entry : fizMap[bucketIndex]) {
+            if (entry.first == key) {
+                // found
+                swap(entry,fizMap[bucketIndex].back());
+                fizMap[bucketIndex].pop_back();
+                return;
+            }
+        }
     }
-  }
 };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
 
 int main() {
   MyHashMap hm;
